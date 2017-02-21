ad73919d619f78c0b4a6a689e27f9b353498fc97
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2017-02-21 16:53:55 +0000

    Adding the person page.

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,12 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.get('/v1/person', function (req, res, next) {
+  req.data = req.data || { };
+  req.data = { nino: req.query.nino };
+  next();
+})
+
 // add your routes here
 
 module.exports = router
