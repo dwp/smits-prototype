b2d56332e40647b35b09a85d444c0a945fb4ecb6
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2017-03-17 11:20:34 +0000

    Second version of the prototype see front page for details of changes.

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,18 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
-router.get('/v1/person*', function (req, res, next) {
+router.get('/v*/dosearch/',function(req,res,next)
+{
+  var v = req.params[0];
+  if (req.query.nino == 'QQ123456C') res.redirect('/v'+v+"/person-1?nino=QQ123456C")
+  else if (req.query.nino == 'GB654321C') res.redirect('/v'+v+"/person-2?nino=GB654321C")
+  else if (req.query.nino == 'ZZ918273C') res.redirect('/v'+v+"/person-3?nino=ZZ918273C")
+  else {
+    next();
+  }
+});
+
+router.get('/v*/person*', function (req, res, next) {
   req.data = req.data || { };
   req.data = { nino: req.query.nino };
   next();
